04c488db5ea451d443fdefeaab9a213aa3981bb4
Dujthep <dujthep.stop@gmail.com>
2017-03-09 22:58:36 +0700

    v1.1.2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-
 var url = require('url');
 var request = require('request');
 var URL = require('url-parse');
 var express = require('express');
 var app = express();
 var crawler = require('./crawler.js');
-var client  = require('./elastic.js');
+var elast = require('./elastic.js');
 
-client.cluster.health({},function(err,resp,status) {
-  console.log("-- Client Health --",resp);
+elast.cluster.health({}, function(err, resp, status) {
+    console.log("-- Client Health --", resp);
 });
 
 app.get('/temp', function(req, res) {
@@ -19,7 +18,7 @@ app.get('/temp', function(req, res) {
 app.get('/', function(req, res) {
     var tmp = crawler.getData('http://www.thairath.co.th/content/3');
     console.log(JSON.stringify(tmp));
-  //  res.send(tmp);
+    //  res.send(tmp);
 });
 
 app.listen(3000, function() {
