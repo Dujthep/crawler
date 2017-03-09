b861781f0d4e908cc254e11cc4324d2d53a8fe57
Jester'No <sothon11399@gmail.com>
2017-03-09 23:19:57 +0700

    v.1.2
    success callback

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ app.get('/temp', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-    var tmp = crawler.getData('http://www.thairath.co.th/content/3');
-    console.log(JSON.stringify(tmp));
-  //  res.send(tmp);
+    crawler.getData('http://www.thairath.co.th/content/3', function(err, content) {
+        if (err) {
+            console.error("Got an error", err);
+        } else {
+            console.log(content);
+        }
+    });
 });
 
 app.listen(3000, function() {
